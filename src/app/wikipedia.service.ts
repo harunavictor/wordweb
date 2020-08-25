bd036ae92b391041acea063d89f2df52fd2cb68a
harunavictor <44639372+harunavictor@users.noreply.github.com>
2020-08-25 21:05:10 +0100

    first commit

---
+++ src/app/wikipedia.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+
+interface WikipediResponse{
+  query: {
+    search: {
+      pageid: number,
+      title: string,
+      snippet: string
+      }[]
+  };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WikipediaService {
+  constructor(private http: HttpClient) {}
+
+  search(term: string) {
+    return this.http.get<WikipediResponse>('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*'
+      },
+    }).pipe(pluck('query','search'))
+  }
+}
